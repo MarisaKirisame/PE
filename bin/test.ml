c00fd409a78ac96dd3b36320616425f057d6290f
Marisa Kirisame <lolisa@marisa.moe>
2019-02-15 10:16:32 -0800

    save

--- bin/test.ml
+++ bin/test.ml
@@ -1,125 +1,165 @@
 open PE.Common;;
 open Trx;;
 
-module type Lang = sig
-  type 'a repr
-  type 'a value = 'a repr
-  type 'a var = 'a repr
-  val lam_: ('a value -> 'b repr) -> ('a -> 'b) repr
-  val app_: ('a -> 'b) repr -> ('a repr -> 'b repr)
-  val let_: 'a repr -> ('a value -> 'b repr) -> 'b repr
-  val int_: int -> int repr
-  val add_: int repr -> int repr -> int repr
-  val mkRef_: 'a repr -> 'a ref repr
-  val getRef_: 'a ref repr -> 'a repr
-  val setRef_: 'a ref repr -> 'a repr -> unit repr
-  val mkProd_: 'a repr -> 'b repr -> ('a * 'b) repr
-  val zro_: ('a * 'b) repr -> 'a repr
-  val fst_: ('a * 'b) repr -> 'b repr
-  val left_: 'a repr -> ('a, 'b) sum repr
-  val right_: 'b repr -> ('a, 'b) sum repr
-  val match_: ('a, 'b) sum repr -> ('a -> 'c) repr -> ('b -> 'c) repr -> 'c repr
-end;;
+type var = Var of int
 
-module HalfDone = struct
-  let lam_ _ = raise NYI
-  let app_ _ = raise NYI
-  let let_ _ = raise NYI
-  let int_ _ = raise NYI
-  let add_ _ = raise NYI
-  let mkRef_ _ = raise NYI
-  let getRef_ _ = raise NYI
-  let setRef_ _ = raise NYI
-  let mkProd_ _ = raise NYI
-  let zro_ _ = raise NYI
-  let fst_ _ = raise NYI
-  let left_ _ = raise NYI
-  let right_ _ = raise NYI
-  let match_ _ = raise NYI
-end;;
+type term =
+  | FromVar of var
+  | Abs of (var * term)
+  | App of (term * term)
+  | Let of (var * term * term)
+  | Int of int
+  | Add of (term * term)
+  | MkRef of term
+  | GetRef of term
+  | SetRef of (term * term)
+  | MkProd of (term * term)
+  | Zro of term
+  | Fst of term
+  | Left of term
+  | Right of term
+  | Match of term * term * term
+  | Unit
 
-module Eval:Lang = struct
-  type 'a repr = 'a
-  type 'a value = 'a repr
-  let lam_ f = f
-  let app_ f x = f x
-  let let_ x f = f x
-  let int_ x = x
-  let add_ = (+)
-  let mkRef_ x = ref x
-  let getRef_ x = !x
-  let setRef_ x y = x := y
-  let mkProd_ x y = (x, y)
-  let zro_ (x, _) = x
-  let fst_ (_, y) = y
-  let left_ x = Left x
-  let right_ x = Right x
-  let match_ s l r =
-    match s with
-    | Left x -> l x
-    | Right x -> r x
-end;;
+let fresh = ref 0
 
-exception NYI;;
+let genFresh() =
+  let ret = !fresh in
+  fresh := ret + 1;
+  Var ret
 
-module Code: Lang = struct
-  type 'a repr = 'a code
-  type 'a value = 'a repr
-  let lam_ f = .<fun x -> .~(f .<x>.)>.
-  let app_ f x = .<.~f .~x>.
-  let let_ x f = .<let y = .~x in .~(f .<y>.)>.
-  let int_ x = .<x>.
-  let add_ x y = .<.~x + .~y>.
-  let mkRef_ x = .<ref .~x>.
-  let getRef_ x = .<!(.~x)>.
-  let setRef_ x y = .<(.~x):=(.~y)>.
-  let mkProd_ x y = .<(.~x, .~y)>.
-  let zro_ x = .<fst .~x>.
-  let fst_ x = .<snd .~x>.
-  let left_ x = .<Left .~x>.
-  let right_ x = .<Right .~x>.
-  let match_ s l r = .<
-    match .~s with
-    | Left x -> .~l x
-    | Right x -> .~r x>.
-end;;
+type letList = (term -> term) ref
 
-module PE(L: Lang): Lang = struct
-  include HalfDone
-  type 'a shared = Return of 'a | Let: 'a L.repr * ('a L.value -> 'b shared) -> 'b shared
-  let rec shared_join =
-    function
-    | Return a -> a
-    | Let (a, f) -> Let (a, fun a -> shared_join (f a))
-  (*this is a free monad.
-    might do asymptotic improvement of computations over free monad
-    to improve the performance.*)
-  let rec shared_fmap f =
-    function
-    | Return a -> f a
-    | Let (a, g) -> Let (a, fun a -> shared_fmap f (g a))
-  let rec shared_bind a m =
-    match a with
-    | Return a -> m a
-    | Let (a, f) -> Let (a, fun a -> shared_bind (f a) m)
-  type
-    'a static =
-    | SInt: int -> int static
-    | SProd: ('a ps * 'b ps) -> ('a * 'b) static
-  and
-    'a ps = PS of 'a static option * 'a L.repr
-  type 'a repr = 'a ps
-  type 'a value = 'a repr
-  let dyn = function | PS (_, x) -> x
-  let lam_ f = PS (None, L.lam_ (fun x -> dyn (f (PS (None, x)))))
-  let app_ (PS (_, f)) (PS (_, x)) = PS (None, L.app_ f x)
-  let let_ x f = f x
-  let int_ x = PS (Some (SInt x), L.int_ x)
-  let add_ (PS (sl, dl)) (PS (sr, dr)) =
-    let sip =
-      function
-      | (Some (SInt x), Some (SInt y)) -> Some (SInt (x + y))
-      | _ -> None
-    in
-    PS (sip (sl, sr), L.add_ dl dr)
-end;;
+let push l x =
+  let lv = !l in
+  let v = genFresh() in
+  l := (fun t -> lv(Let(v, x, t)));
+  FromVar v
+
+type value =
+  | VFun of (value -> value)
+  | VInt of int
+  | VRef of value ref
+  | VProd of value * value
+  | VSum of (value, value) sum
+  | VUnit
+
+(*Our partial evaluator has almost the same value as the ordinary evaluator.
+  the only problem is that reference environment might be touched by unknown SetRef/App,
+  so we have to modify it by keeping a time ref denoting the last time it was touched.
+  The static ref will include a timestamp denoting the last valid time.
+
+  Since duplication with effect is semantically incorrect, dynVal will hold only value.
+  Computation must be pushed to a letList ref.
+*)
+type time = int
+
+type
+  static =
+  | SFun of (letList -> pValue -> pValue)
+  | SInt of int
+  | SRef of (pValue * time) ref
+  | SProd of pValue * pValue
+  | SSum of (pValue, pValue) sum
+  | SUnit
+and
+  pValue =
+  { pStatic: static option;
+    dynVal: term }
+
+let fresh: int ref = ref 0
+
+type 'a env = var -> 'a
+
+let extend e (Var v) x =
+  function
+  | Var i when i == v -> x
+  | i -> e i
+
+let incTime x =
+  x := (!x) + 1
+
+let static s d = { pStatic = Some s; dynVal = d }
+
+let staticInt s = static (SInt s) (Int s)
+
+let dynamic d = { pStatic = None; dynVal = d }
+
+let rec peAux(curTime: time ref)(e: pValue env)(l : letList): term -> pValue =
+  let recurse = peAux curTime e l in
+  let app x y =
+    match x.pStatic with
+    | Some (SFun f) -> f l y
+    | _ -> incTime curTime; dynamic (push l (App (x.dynVal, y.dynVal)))
+  in
+  function
+  | Int i -> staticInt i
+  | Add (x, y) ->
+    let px = recurse(x) in
+    let py = recurse(y) in
+    (match (px.pStatic, py.pStatic) with
+    | (Some (SInt x), Some (SInt y)) -> staticInt (x + y)
+    | _ -> dynamic (push l (Add (px.dynVal, py.dynVal))))
+  | MkProd (x, y) ->
+    let px = recurse(x) in
+    let py = recurse(y) in
+    static (SProd (px, py)) (push l (MkProd (px.dynVal, py.dynVal)))
+  | Zro x ->
+    let px = recurse(x) in
+    (match px.pStatic with
+     | Some (SProd (x, _)) -> x
+     | _ -> dynamic (push l (Zro (px.dynVal))))
+  | Fst x ->
+    let px = recurse(x) in
+    (match px.pStatic with
+     | Some (SProd (_, y)) -> y
+     | _ -> dynamic (push l (Fst (px.dynVal))))
+  | Left x ->
+    let px = recurse(x) in
+    static (SSum (Left px)) (push l (Left px.dynVal))
+  | Right x ->
+    let px = recurse(x) in
+    static (SSum (Right px)) (push l (Right px.dynVal))
+  | App (f, x) ->
+    let pf = recurse(f) in
+    let px = recurse(x) in
+    app pf px
+  | Match (s, lcase, rcase) ->
+    let ps = recurse(s) in
+    let pl = recurse(lcase) in
+    let pr = recurse(rcase) in
+    (match ps.pStatic with
+    | (Some (SSum (Left x))) -> app pl x
+    | (Some (SSum (Right x))) -> app pr x
+    | _ -> incTime curTime; dynamic (push l (Match (ps.dynVal, pl.dynVal, pr.dynVal))))
+  | MkRef x ->
+    let px = recurse(x) in
+    static (SRef (ref (px, !curTime))) (push l (MkRef px.dynVal))
+  | GetRef r ->
+    let pr = recurse(r) in
+    (match pr.pStatic with
+     | Some (SRef { contents = (px, time) }) when !curTime == time -> px
+     | _ -> dynamic (push l (GetRef pr.dynVal)) )
+  | SetRef (r, v) ->
+    let pr = recurse(r) in
+    let pv = recurse(v) in
+    (match pr.pStatic with
+     | Some (SRef r) -> r := (pv, !curTime)
+     | _ -> let _ = push l (SetRef (pr.dynVal, pv.dynVal)) in incTime curTime);
+    static SUnit Unit
+  | Unit -> static SUnit Unit
+  | FromVar v -> e v
+  | Let (var, v, body) -> peAux(curTime)(extend e var (recurse(v)))(l)(body)
+  | Abs e -> static (SFun (fun l p ->
+      { pStatic = p.pStatic; dynVal = push l p.dynVal })) (Abs e)
+
+exception NotFound;;
+
+let pe x =
+  let
+    ll = ref (fun x -> x)
+  in
+  let
+    res = peAux(ref 0)(fun _ -> raise NotFound)(ll) x
+  in
+  (!ll) res.dynVal
